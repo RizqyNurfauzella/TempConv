59bf03e32216b374cef53113d946fac82b6baac2
iqbaalhibatulloh <iqbaalhibatulloh03@gmail.com>
2024-10-25 15:18:49 +0700

    Add: setup navgraph for soon and object screen and add pager using panel, page: setting, temp, home

---
+++ app/src/main/java/org/d3ifcool/tempconv/navigation/NavigationGraph.kt
@@ -0,0 +1,29 @@
+package org.d3ifcool.tempconv.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import org.d3ifcool.tempconv.tabpanel.Home
+import org.d3ifcool.tempconv.tabpanel.Setting
+import org.d3ifcool.tempconv.tabpanel.Temperature
+
+@Composable
+fun NavigationGraph(navController: NavHostController = rememberNavController()) {
+   NavHost(
+      navController = navController,
+      startDestination = Screen.Home.route
+   ) {
+      // Main route
+      composable(route = Screen.Home.route) {
+         Home(navController)
+      }
+      composable(route = Screen.Temperature.route) {
+         Temperature(navController)
+      }
+      composable(route = Screen.Setting.route) {
+         Setting(navController)
+      }
+   }
+}
